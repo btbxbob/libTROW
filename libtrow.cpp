5ea64751796196c94670328d6454f61194355140
BX <btbxbob@gmail.com>
2013-09-15 10:09:06 +0800

    start writing

--- libtrow.cpp
+++ libtrow.cpp
@@ -1,6 +1,11 @@
 #include "libtrow.h"
+#include <QtNetwork>
+#include <QJsonArray>
+#include <QJsonObject>
+#include <QJsonDocument>
 
 
 LibTROW::LibTROW()
 {
+
 }
--- libtrow.h
+++ libtrow.h
@@ -2,12 +2,48 @@
 #define LIBTROW_H
 
 #include "libtrow_global.h"
+#include <QString>
 
+/*!
+ * \brief The LibTROW class
+ * \link http://trow.cc/wiki/trow/api/start
+ */
 class LIBTROWSHARED_EXPORT LibTROW
 {
 
 public:
+    /*!
+     * \brief The Respond class
+     * 服务器回复的类，似乎只有错误信息呢。
+     */
+    class Respond
+    {
+        short error;
+        QString message;
+    };
+
+    /*!
+     * \brief The Stats class
+     * 存放从服务器返回的t，也就是时间，注意有超时
+     */
+    class Stats :Respond
+    {
+        short t;
+    };
+
+    class Request
+    {
+        QString appkey;
+        QString apptoken;
+        short t;
+    };
+
     LibTROW();
+    Stats stats;
+
+private:
+    bool is_timeout();
+
 };
 
 #endif // LIBTROW_H
